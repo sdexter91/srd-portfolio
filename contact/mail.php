7a65051c9fc775ce124949acb6ccc3f7fa75eb47
Sam Dexter <sdexter91@gmail.com>
2020-07-28 16:44:01 +0200

    php email changed to sam@samrdexter.com

--- contact/mail.php
+++ contact/mail.php
@@ -4,7 +4,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "sdexter91@yahoo.com";
+$recipient = "sam@samrdexter.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader);
